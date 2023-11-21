a3b51c30d94319a7c815f7b1531be779c39d5bf6
luffy <ilyagt@gmail.com>
2023-11-21 15:25:28 +1000

    Initial commit

---
+++ src/main/java/dev/steadypim/multimodalb2bshipmentdiploma/MultimodalB2bShipmentDiplomaApplication.java
@@ -0,0 +1,13 @@
+package dev.steadypim.multimodalb2bshipmentdiploma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultimodalB2bShipmentDiplomaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultimodalB2bShipmentDiplomaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/steadypim/multimodalb2bshipmentdiploma/MultimodalB2bShipmentDiplomaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.steadypim.multimodalb2bshipmentdiploma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultimodalB2bShipmentDiplomaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
